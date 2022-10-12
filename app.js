3f4ac7bb34c9ce7f50f358745e289ac9133882c5
Han Xian Xu Huang <howie010103@gmail.com>
2022-10-12 16:21:47 +0000

    Disable x-powered-by header for security

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.set('trust proxy', 1); // get the real ip address
+app.disable('x-powered-by');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,14 +11,14 @@ app.use(express.json());
 // Take down the server
 app.use(function (req, res, next) {
     let server_down = false;
-    
-    if (server_down){
-        if (req.ip !== "127.0.0.1" && req.ip.substring(7) !== "127.0.0.1"){
-            res.status(503).json({message: "We are making the server better! Please try again later."});
+
+    if (server_down) {
+        if (req.ip !== "127.0.0.1" && req.ip.substring(7) !== "127.0.0.1") {
+            res.status(503).json({ message: "We are making the server better! Please try again later." });
             return;
         }
     }
-    
+
     next();
 });
 
@@ -46,7 +47,7 @@ const rate_limit = require("./setup/rate_limit");
 const studentsRouter = require('./routes/student');
 const interviewsTaRouter = require('./routes/ta');
 const adminsRouter = require('./routes/admin');
- 
+
 app.get("/", rate_limit.general_limiter, (req, res) => {
     res.status(418).json({ message: "Why are you here??? LOL --Howie" });
 })
@@ -55,22 +56,26 @@ app.use('/ta', interviewsTaRouter);
 app.use('/admin', adminsRouter);
 
 // Error handling
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Invalid URL or request method." });
+})
+
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) {
         if (err.code === "LIMIT_FILE_SIZE") {
             res.status(406).json({ message: "File is too large." });
         } else if (err.code === "LIMIT_UNEXPECTED_FILE") {
-            res.status(400).json({ message: "Unexpected file is uploaded. Please check if your URL is correct." });
+            res.status(400).json({ message: "Unexpected file is uploaded. Please check if your URL and request method are correct." });
         } else {
             console.error(err);
             console.log(req);
-            res.status(501).json({ message: "Upload fails. Please check error log." });
+            res.status(501).json({ message: "Upload fails. Please contact system administrator." });
         }
     } else if (err instanceof SyntaxError) {
         return res.status(400).send({ message: "Invalid data is received." });
     } else {
         console.error(err);
-        res.status(500).json({ message: "Unknown error. Please check error log." });
+        res.status(500).json({ message: "Unknown error. Please contact system administrator." });
     }
 })
 
